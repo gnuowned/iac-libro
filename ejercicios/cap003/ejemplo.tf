72f383428dbf514cb52cce128f2d02e26c5af910
Miguel Barajas <migbaraj@cisco.com>
2021-02-05 12:41:45 -0600

    primer ejemplo de tf

---
+++ ejercicios/cap003/ejemplo.tf
@@ -0,0 +1,18 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 2.70"
+    }
+  }
+}
+
+provider "aws" {
+  profile = "default"
+  region  = "us-west-2"
+}
+
+resource "aws_instance" "example" {
+  ami           = "ami-830c94e3"
+  instance_type = "t2.micro"
+}
